10a0989e7f106f1239a3f47a454c7786a0ae4d3f
andy <andytwoods@gmail.com>
2020-02-14 20:04:10 +0000

    mods

--- lib/main.dart
+++ lib/main.dart
@@ -52,17 +52,22 @@ class _MyHomePageState extends State<MyHomePage> {
 
   final TextEditingController _controller = TextEditingController();
   final GlobalKey _textFieldKey = GlobalKey();
+  final double MaxFontSize = 200;
+  final double MinFontSize = 2;
 
   double _fontSize = textFieldTextStyle.fontSize;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
+
+    double getFontSize() {
+      double _updatedFontSize = _fontSize * _scale;
+      if(_updatedFontSize < MinFontSize) return MinFontSize;
+      if(_updatedFontSize > MaxFontSize) return MaxFontSize;
+      return  _updatedFontSize;
+    }
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -75,25 +80,18 @@ class _MyHomePageState extends State<MyHomePage> {
             _scale = scaleDetails.scale;
           });
         },
+        onScaleEnd:  (ScaleEndDetails scaleDetails) {
+          setState(() {
+            _fontSize = _scale * _fontSize;
+            _scale = 1;
+          });
+        },
         child: Center(
           // Center is a layout widget. It takes a single child and positions it
           // in the middle of the parent.
           child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
             mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               Expanded(
                 child: TextField(
@@ -102,12 +100,14 @@ class _MyHomePageState extends State<MyHomePage> {
                   key: _textFieldKey,
                   controller: _controller,
                   decoration: InputDecoration(
+                    hintText: 'Start typing. Try zooming...',
+                    hintStyle: TextStyle(fontSize: 25),
                     border: InputBorder.none,
                     fillColor: Colors.orange,
                     filled: true,
-                    contentPadding: textFieldPadding,
+                    //contentPadding: textFieldPadding,
                   ),
-                  style: textFieldTextStyle.copyWith(fontSize: _fontSize * _scale),
+                  style: textFieldTextStyle.copyWith(fontSize: getFontSize()),
                 ),
               )
             ],
@@ -115,8 +115,11 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        tooltip: 'Clear text',
+        child: Icon(Icons.clear),
+        onPressed: (){
+          _controller.clear();
+        },
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
